4be6499b113af243e345e6a4d605e9f65e7874e4
Israa <32584309+Esra-Alhussain@users.noreply.github.com>
2024-05-26 22:18:02 -0700

    Initialize Amplify in client directory

---
+++ client/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "achivermomentss5a0d034a": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
