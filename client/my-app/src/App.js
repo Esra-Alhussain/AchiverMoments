aa8dcb5a5137ae04d34695cb94f3e3333600e312
Israa <32584309+Esra-Alhussain@users.noreply.github.com>
2024-05-26 22:43:11 -0700

    Create App component with routing and authentication

--- client/my-app/src/App.js
+++ client/my-app/src/App.js
@@ -1,10 +1,22 @@
-import './App.css';
+import "./App.css";
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { withAuthenticator } from "@aws-amplify/ui-react";
+import Home from "./components/Home";
+import AddAchievement from "./components/AddAchievement";
+import SignIn from "./components/SignIn";
+import SignUp from "./components/SignUp";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Welcome</h1>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/add" component={AddAchievement} />
+        <Route path="/signin" component={SignIn} />
+        <Route path="/signup" component={SignUp} />
+      </Switch>
+    </Router>
   );
 }
 
